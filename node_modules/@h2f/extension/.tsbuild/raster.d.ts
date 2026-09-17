ce02e3043b705b95c341c5dcc976bf9d25b0bae4
ChristianRukundo <siboruremarukundoc1@gmail.com>
2026-09-17 23:33:01 +0200

    Exclude local build artifacts

--- node_modules/@h2f/extension/.tsbuild/raster.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Capture, Warning } from '@h2f/schema';
-export interface RasterOptions {
-    tabId: number;
-    windowId: number;
-    onProgress?: (done: number, total: number) => void;
-    isCancelled?: () => boolean;
-}
-export declare function rasterizeMarked(capture: Capture, options: RasterOptions): Promise<Warning[]>;
-//# sourceMappingURL=raster.d.ts.map
--- node_modules/@h2f/plugin/.tsbuild/messages.js
+++
@@ -1,5 +0,0 @@
-export const DEFAULT_IMPORT_OPTIONS = {
-    autoLayout: true,
-    createStyles: false,
-};
-//# sourceMappingURL=messages.js.map
